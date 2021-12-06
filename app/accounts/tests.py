594dc7d18acc4d24b4b5e58ef39b86abbcee0c85
hinorahere <hinorahere@gmail.com>
2021-12-06 12:11:12 -0800

    Clean up testing

--- app/accounts/tests.py
+++ app/accounts/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """
+        Test new user creation with an email is successful
+        """
+        username = 'adminAdmin123'
+        email = 'test@gmail.com'
+        password = 'Password123'
+        user = get_user_model().objects.create_user(
+            username=username,
+			email=email,
+			password=password
+		)
+
+        self.assertEqual(user.username, username)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
