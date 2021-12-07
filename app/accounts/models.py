da631327a74e7b98d85a71595eaa5d2741312d0b
hinorahere <hinorahere@gmail.com>
2021-12-07 14:12:18 -0800

    Basic profile setup. ForeignKey added to user model

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 
 from .managers import CustomUserManager
+from profiles.models import Profile
 
 
 class CustomUser(AbstractUser):
@@ -13,6 +14,11 @@ class CustomUser(AbstractUser):
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
+
+    profile = models.ForeignKey(Profile,
+                                on_delete=models.CASCADE,
+                                blank=True,
+                                null=True)
 
     def __str__(self):
         return self.email
