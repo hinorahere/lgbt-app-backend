7cfa4dc19be3a996abb61e09f6a1f063cd5fd640
hinorahere <hinorahere@gmail.com>
2021-12-07 14:53:26 -0800

    Profile endpoints created

---
+++ app/profiles/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Profile
+
+
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Profile
+        fields = ('id',
+                  'first_name',
+                  'last_name',
+                  'bio', )
