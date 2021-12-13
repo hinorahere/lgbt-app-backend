ef89493b46c6f66d5c5fa603c9404590d288d348
hinorahere <hinorahere@gmail.com>
2021-12-13 12:44:18 -0800

    Quick adin changes

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import CustomUser
 
 # Register your models here.
 class CustomUserAdmin(admin.ModelAdmin):
-    fields = ('email',
+    fields = ('username',
+              'email',
               'profile',
               'matches',
               'prospects',
