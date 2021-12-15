5707a3124f329b83d1f7d0b2e86c2bd7ea8ffaa7
hinorahere <hinorahere@gmail.com>
2021-12-15 14:27:02 -0800

    Matching functionality complete.

--- app/matches/views.py
+++ app/matches/views.py
@@ -12,22 +12,35 @@ from accounts.models import CustomUser as User
 @api_view(['POST'])
 def match(request):
     if request.method == 'POST':
+        # Type check(s)
         if not isinstance(request.data["prospective_id"], int):
             raise TypeError("prospective_id must be of type int")
 
+        # Get current and prospective users
         try:
-            current_user = User.objects.get(id=request.user.id)
             prospective_id = int(request.data["prospective_id"])
+            prospective_user = User.objects.get(id=prospective_id)
+            current_user_id = request.user.id
+            current_user = User.objects.get(id=current_user_id)
         except Exception as error:
             raise Exception(repr(error))
 
+        # Determine if users are a match
         match = False
-        for prospect in current_user.prospects.all():
-            if int(str(prospect)) == prospective_id:
+        for user_id in prospective_user.prospects.all():
+            if int(str(user_id)) == current_user_id:
                 match = True
                 break
 
-        return Response(match)
+        # If match, update match fields on both users
+        # else add prospective user to current_users prospective list
+        if match:
+            current_user.matches.add(prospective_user)
+            prospective_user.matches.add(current_user)
+        else:
+            current_user.prospects.add(prospective_user)
+
+        return Response([int(str(val)) for val in current_user.matches.all()])
 
 
 class MatchList(APIView):
