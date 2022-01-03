5e92d8c9e66b23f5ad05efe665e581557c88c519
Nora Renfrow <25420200+hinorahere@users.noreply.github.com>
2022-01-03 14:47:18 -0800

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
-
-ALLOWED_HOSTS = []
+# FIX THIS**********************************************************************************************
+ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS.extend(
     filter(
         None,
